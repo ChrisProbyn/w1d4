7b48bafc843d8eee8646d4ea4e28fdb8dcfb0bc4
ChrisProbyn <cprobyn16@gmail.com>
2018-11-29 18:52:58 +0000

    found waldo fix

--- findWaldo.js
+++ findWaldo.js
@@ -1,12 +1,18 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-    }
-  }
+      var i = 0;
+      arr.forEach( function (element,index){
+        //console.log(element, index);
+          if (element === "Waldo") {
+         found(index);
+            }
+
+      });
+
 }
 
+
 function actionWhenFound(index) {
+
   console.log("Found him!" + " at index: " + index);
 }
 
